7b7a591e294a1e34ecb0889301e6549e4da42aca
Carl Wiedemann <carl.wiedemann@gmail.com>
2023-01-08 19:54:25 -0700

    Initial commit

---
+++ lib/foresite.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "foresite/version"
+
+module Foresite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/foresite_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Foresite do
+  it "has a version number" do
+    expect(Foresite::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
