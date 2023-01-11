9620cca74b22c6cec76b01e45a50a7178997a4cc
Carl Wiedemann <carl.wiedemann@gmail.com>
2023-01-11 06:56:12 -0700

    testing github actions

--- lib/foresite/cli.rb
+++ lib/foresite/cli.rb
@@ -3,8 +3,15 @@ module Foresite
 
     desc "pluralize", "Pluralizes a word"
     method_option :word, aliases: "-w"
+
     def pluralize(name)
       puts "hello"
+    end
+
+    desc "say_hello NAME", "say hello to NAME"
+
+    def say_hello(name)
+      puts "Hello #{name}"
     end
 
   end
